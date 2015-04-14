a746c1a5a25b889cad7393eb594f88c9f84bdbe5
Nikhil Mehta <nikhilmehta1901@gmail.com>
2015-04-14 23:29:41 +0530

    Create Stickers.cpp

---
+++ Stickers.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T;
+    unsigned N,temp;
+    cin>>T;
+    while(T--)
+    {
+    	cin>>N;
+    	temp=(N*(N+1))/2;
+    	cout<<temp<<endl;
+    	
+    }
+    return 0;
+}
